65aa0e37b765b4d54249d2685bc58ba33924e7d2
blank-27 <55805028+blank-27@users.noreply.github.com>
2020-02-07 23:28:20 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,21 +2,21 @@
     function validateuser() {
         var username1 = document.getElementsByClassName("username1").value;
         var password1 = document.getElementsByClassName("password1").value;
-        if (username1 == " " && password1 == " ") {
+        if (username1 === " " && password1 === " ") {
             alert("username or password missing!!");
         } else {
             alert("Login successfully");
-            window.location = "./sucess1.html";
+            window.location = "sucess1.html";
         }
     }
 
 function validatemod() {
     var username2 = document.getElementsByClassName("username2").value;
     var password2 = document.getElementsByClassName("password2").value;
-    if (username2 == " " && password2 == " ") {
+    if (username2 === " " && password2 === " ") {
         alert("username or password missing!!");
     } else {
         alert("Login successfully");
-        window.location = "./sucess2.html";
+        window.location = "sucess2.html";
     }
 }
